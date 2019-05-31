9950ea89febb0511381150a2b666d4552c928969
whdtjr3973 <whdtjr3973@naver.com>
2019-05-31 16:37:08 +0900

    first

---
+++ src/Gugu_01.java
@@ -0,0 +1,127 @@
+
+public class Gugu_01 {
+	
+	public static void main(String[] args) {
+		
+		int num1 = 1;
+		
+		int num2 = num1 + 1;
+		System.out.println("========== 3단 ==========");
+		System.out.println("3 x " + num2 + " = " + (3 * num2));
+		num2 = num1 + 2;
+		System.out.println("3 x " + num2 + " = " + (3 * num2));
+		num2 = num1 + 3;
+		System.out.println("3 x " + num2 + " = " + (3 * num2));
+		num2 = num1 + 4;
+		System.out.println("3 x " + num2 + " = " + (3 * num2));
+		num2 = num1 + 5;
+		System.out.println("3 x " + num2 + " = " + (3 * num2));
+		num2 = num1 + 6;
+		System.out.println("3 x " + num2 + " = " + (3 * num2));
+		num2 = num1 + 7;
+		System.out.println("3 x " + num2 + " = " + (3 * num2));
+		num2 = num1 + 8;
+		System.out.println("3 x " + num2 + " = " + (3 * num2));
+		
+		System.out.println("========== 3단 ==========");
+		num2 = num1+1;
+		System.out.println(num1+2+ "x" + num2 + "=" + (num1+2)*num2 );
+		num2 = num1+2;
+		System.out.println(num1+2+ "x" + num2 + "=" + (num1+2)*num2 );
+		num2 = num1+3;
+		System.out.println(num1+2+ "x" + num2 + "=" + (num1+2)*num2 );
+		num2 = num1+4;
+		System.out.println(num1+2+ "x" + num2 + "=" + (num1+2)*num2 );
+		num2 = num1+5;
+		System.out.println(num1+2+ "x" + num2 + "=" + (num1+2)*num2 );
+		num2 = num1+6;
+		System.out.println(num1+2+ "x" + num2 + "=" + (num1+2)*num2 );
+		num2 = num1+7;
+		System.out.println(num1+2+ "x" + num2 + "=" + (num1+2)*num2 );
+		num2 = num1+8;
+		System.out.println(num1+2+ "x" + num2 + "=" + (num1+2)*num2 );
+		
+		
+		
+		System.out.println("========== 4단 ==========");
+		num2 = num1 + 1;
+		System.out.println("4 x " + num2 + " = " + (4 * num2));
+		num2 = num1 + 2;
+		System.out.println("4 x " + num2 + " = " + (4 * num2));
+		num2 = num1 + 3;
+		System.out.println("4 x " + num2 + " = " + (4 * num2));
+		num2 = num1 + 4;
+		System.out.println("4 x " + num2 + " = " + (4 * num2));
+		num2 = num1 + 5;
+		System.out.println("4 x " + num2 + " = " + (4 * num2));
+		num2 = num1 + 6;
+		System.out.println("4 x " + num2 + " = " + (4 * num2));
+		num2 = num1 + 7;
+		System.out.println("4 x " + num2 + " = " + (4 * num2));
+		num2 = num1 + 8;
+		System.out.println("4 x " + num2 + " = " + (4 * num2));
+		
+		System.out.println("========== 4단 ==========");
+		num2 = num1+1;
+		System.out.println(num1+3+ "x" + num2 + "=" + (num1+3)*num2 );
+		num2 = num1+2;
+		System.out.println(num1+3+ "x" + num2 + "=" + (num1+3)*num2 );
+		num2 = num1+3;
+		System.out.println(num1+3+ "x" + num2 + "=" + (num1+3)*num2 );
+		num2 = num1+4;
+		System.out.println(num1+3+ "x" + num2 + "=" + (num1+3)*num2 );
+		num2 = num1+5;
+		System.out.println(num1+3+ "x" + num2 + "=" + (num1+3)*num2 );
+		num2 = num1+6;
+		System.out.println(num1+3+ "x" + num2 + "=" + (num1+3)*num2 );
+		num2 = num1+7;
+		System.out.println(num1+3+ "x" + num2 + "=" + (num1+3)*num2 );
+		num2 = num1+8;
+		System.out.println(num1+3+ "x" + num2 + "=" + (num1+3)*num2 );
+		
+		
+		
+		int intGu = 3;
+		int intEle = 2;
+		System.out.println("---------- 3단 ----------");
+		System.out.println(intGu + "x" + intEle + "=" + intGu*intEle);
+		
+		intEle = intEle+1;
+		System.out.println(intGu + "x" + intEle + "=" + intGu*intEle);
+		intEle = intEle+1;
+		System.out.println(intGu + "x" + intEle + "=" + intGu*intEle);
+		intEle = intEle+1;
+		System.out.println(intGu + "x" + intEle + "=" + intGu*intEle);
+		intEle = intEle+1;
+		System.out.println(intGu + "x" + intEle + "=" + intGu*intEle);
+		intEle = intEle+1;
+		System.out.println(intGu + "x" + intEle + "=" + intGu*intEle);
+		intEle = intEle+1;
+		System.out.println(intGu + "x" + intEle + "=" + intGu*intEle);
+		intEle = intEle+1;
+		System.out.println(intGu + "x" + intEle + "=" + intGu*intEle);
+		
+		intGu = intGu+1;
+		intEle = 2;
+		System.out.println("---------- 4단 ----------");
+		System.out.println(intGu + "x" + intEle + "=" + intGu*intEle);
+		intEle = intEle+1;
+		System.out.println(intGu + "x" + intEle + "=" + intGu*intEle);
+		intEle = intEle+1;
+		System.out.println(intGu + "x" + intEle + "=" + intGu*intEle);
+		intEle = intEle+1;
+		System.out.println(intGu + "x" + intEle + "=" + intGu*intEle);
+		intEle = intEle+1;
+		System.out.println(intGu + "x" + intEle + "=" + intGu*intEle);
+		intEle = intEle+1;
+		System.out.println(intGu + "x" + intEle + "=" + intGu*intEle);
+		intEle = intEle+1;
+		System.out.println(intGu + "x" + intEle + "=" + intGu*intEle);
+		intEle = intEle+1;
+		System.out.println(intGu + "x" + intEle + "=" + intGu*intEle);
+		
+		
+		
+	}
+
+}
---
+++ src/Gugu_05.java
@@ -0,0 +1,56 @@
+
+public class Gugu_05 {
+	
+	public static void main(String[] args) {
+		
+		int intGu = 0;
+		int intEle = 0;
+		
+		// ?ê¸°ì?‹ (intGu)?„ 6 ì¦ê??‹œ?‚¤?Š” ?—°?‚°
+		intGu += 6; // intGu = intGu + 6ê³? ê°™ì? ?˜ë¯?
+		intEle = 1;
+		
+		
+		// ì¦ê° ?—°?‚°?
+		// ?ê¸°ì?‹ (intEle)?„ 1 ì¦ê??‹œ?‚¤?Š” ?—°?‚°
+		intEle = intEle + 1;
+		intEle += 1; // ?´?•­?—°?‚°
+		intEle ++ ;
+		++intEle; 
+		
+		// ?ê¸°ì?‹ (intEle)?„ 1 ê°ì†Œ?‹œ?‚¤?Š” ?—°?‚°
+		intEle = intEle -1;
+		intEle -= 1;
+		intEle -- ;
+		--intEle;
+		
+		// ?ê¸°ì?‹ (??)?„ 1?´?•„?‹Œ ?–´?– ?•œ ê°’ìœ¼ë¡? ê°?ê°ìŠ¹? œ?•˜?Š” ?—°?‚°
+		intGu += 3; // intGuë¥? 3 ì¦ê?
+		intGu -= 3; // intGuë¥? 3 ê°ì†Œ
+		intGu *= 3; // intGu = intGu * 3
+		intGu /= 3; // intGu = iutGu / 3
+		intGu %= 2; // intGu = intGu % 2
+		
+		intGu = 5;
+		intEle = 1;
+		
+		intEle ++;
+		System.out.println(intGu + " x " + intEle + " = " + intGu * intEle);
+		intEle ++;
+		System.out.println(intGu + " x " + intEle + " = " + intGu * intEle);
+		intEle ++;
+		System.out.println(intGu + " x " + intEle + " = " + intGu * intEle);
+		intEle ++;
+		System.out.println(intGu + " x " + intEle + " = " + intGu * intEle);
+		intEle ++;
+		System.out.println(intGu + " x " + intEle + " = " + intGu * intEle);
+		intEle ++;
+		System.out.println(intGu + " x " + intEle + " = " + intGu * intEle);
+		intEle ++;
+		System.out.println(intGu + " x " + intEle + " = " + intGu * intEle);
+		intEle ++;
+		System.out.println(intGu + " x " + intEle + " = " + intGu * intEle);
+
+	}
+
+}
